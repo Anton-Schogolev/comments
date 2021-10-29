fc4757eb9407706c8065399303c8dbf7618e988a
Anton-Schogolev <anton.schogolev@gmail.com>
2021-10-30 00:05:37 +0300

    feat: #2 creates Comment component

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, {FC, useState} from 'react';
 import {CommentType} from "../../App";
+import styled from "styled-components";
 
+const CommentContainer = styled.div<{ isFirst: boolean }>`
+  border-left: ${({isFirst}) => !isFirst && "#8597ff solid 1px"};
+  margin: 10px;
+`
 
-
-function Comment({comment, author, parentId, id, childrenCom}: CommentType) {
-
+const Comment: FC<CommentType> = (
+    {
+        comment, author,
+        parentId, id,
+        childrenCom, depth,
+        children
+    }) => {
+    const [showChildren, setShowChildren] = useState(depth !== 2)
+    const isFirst = !depth;
+    const renderChildren = () => {
+        return childrenCom?.map(comment => <Comment {...comment}/>)
+    }
     return (
-        <div>
-            <span><b>{author}</b></span>
-            <p>{comment}</p>
-        </div>
+        <CommentContainer isFirst={isFirst}>
+            <div>
+                <span><b>{author}</b></span>
+                <p>{comment}</p>
+                <button>reply</button>
+            </div>
+            {showChildren
+                ? <>
+                    {childrenCom && <button onClick={() => setShowChildren(false)}>-</button>}
+                    {renderChildren()}
+                </>
+                : <button onClick={() => setShowChildren(true)}>show more</button>
+            }
+        </CommentContainer>
     );
 }
 
